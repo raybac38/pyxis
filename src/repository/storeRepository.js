38f3323c46f5be5f5a950fe974ae099ede44da1b
hugo <hponsin@gmail.com>
2026-05-02 12:53:31 +0200

    store + store brand

---
+++ src/repository/storeRepository.js
@@ -0,0 +1,41 @@
+
+
+class StoreRepository{
+
+
+    constructor(database)
+    {
+        this.database = database;
+    }
+
+    getByName(name) {
+        return this.database.prepare(`
+            SELECT * FROM Store
+            WHERE name == ?`).get(name)
+    }
+
+    getById(id) {
+        return this.database.prepare(`
+            SELECT * FROM Store
+            WHERE id == ?`).get(id)
+    }
+
+    create(brand_id, name, addr, city, latitude, longitude){
+        return this.database.prepare(`
+            INSERT INTO Store (brand_id, name, addr, city, latitude, longitude)
+            VALUES (?,?,?,?,?,?)`).run(brand_id, name, addr, city, latitude, longitude)
+    }
+
+    update(id, brand_id, name, addr, city, latitude, longitude){
+        return this.database.prepare(`
+            UPDATE Store
+            SET brand_id=?, name=?, addr=?, city=?, latitude=?, longitude=?
+            WHERE id == ?`).run(brand_id, name, addr, city, latitude, longitude, id)
+    }
+
+    delete(id){
+        return this.database.prepare(`
+            DELETE FROM Store
+            WHERE id == ?`).run(id)
+    }
+}
